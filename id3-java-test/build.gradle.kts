83ceebdfc9e53af926f6e9a5ae9456e67a7ad68a
Rey <89825425+Kingg22@users.noreply.github.com>
2025-07-26 00:29:55 -0500

    build(vibrion-id3-java-tests): bump to 0.6.0

--- id3-java-test/build.gradle.kts
+++ id3-java-test/build.gradle.kts
@@ -8,7 +8,7 @@ version = "unspecified"
 dependencies {
     // hardcode because https://youtrack.jetbrains.com/issue/KMT-1428/Calling-kotlin-common-code-from-java-overload-and-non-primitive-types-cause-unresolved-overload-ambiguity
     @Suppress("kotlin:S6624")
-    testImplementation("io.github.kingg22:vibrion-id3:0.5.0")
+    testImplementation("io.github.kingg22:vibrion-id3:0.6.0")
     testImplementation(platform(libs.junit.bom))
     testImplementation(libs.junit.jupiter)
     testRuntimeOnly(libs.junit.platform)
