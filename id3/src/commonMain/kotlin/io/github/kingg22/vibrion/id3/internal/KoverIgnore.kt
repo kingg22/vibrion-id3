22ffadadc96840e53789ae057165801d160391f0
Rey <89825425+Kingg22@users.noreply.github.com>
2025-10-10 20:24:57 -0500

    perf(vibrion-id3): [internal] explicit define retention and target for KoverIgnore

--- id3/src/commonMain/kotlin/io/github/kingg22/vibrion/id3/internal/KoverIgnore.kt
+++ id3/src/commonMain/kotlin/io/github/kingg22/vibrion/id3/internal/KoverIgnore.kt
@@ -1,3 +1,5 @@
 package io.github.kingg22.vibrion.id3.internal
 
+@Retention(AnnotationRetention.BINARY)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
 internal annotation class KoverIgnore
