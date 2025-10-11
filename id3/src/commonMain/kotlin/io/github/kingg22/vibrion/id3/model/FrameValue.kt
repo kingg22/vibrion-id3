c8ebc6dd0dfdbb3609a63c8b3ae100150d180f7e
Rey <89825425+Kingg22@users.noreply.github.com>
2025-10-11 13:40:16 -0500

    perf(vibrion-id3): BREAKING FrameValue now is an open interface, move languageFormat to static function

--- id3/src/commonMain/kotlin/io/github/kingg22/vibrion/id3/model/FrameValue.kt
+++ id3/src/commonMain/kotlin/io/github/kingg22/vibrion/id3/model/FrameValue.kt
@@ -1,12 +1,23 @@
 package io.github.kingg22.vibrion.id3.model
 
+import kotlin.jvm.JvmStatic
+
 /**
  * Define Frame Value to use in [io.github.kingg22.vibrion.id3.Id3AudioWriter] set.
  * @author Kingg22
  * @see <a href="https://picard-docs.musicbrainz.org/en/appendices/tag_mapping.html">Tag Mapping</a>
  */
-sealed class FrameValue {
-    protected fun languageFormat(language: String) = require(language.matches(Regex("[a-zA-Z]{3}"))) {
-        "Language must follow ISO 639-2"
+interface FrameValue {
+    /** Utilities for [FrameValue] implementations */
+    companion object {
+        /** Check if language is in ISO 639-2 format.
+         * @throws IllegalArgumentException if language is not in ISO 639-2 format
+         * @see <a href="https://en.wikipedia.org/wiki/List_of_ISO_639-2_codes">List of ISO 639-2 codes</a>
+         * @see <a href="https://id3.org/id3v2.3.0#ID3v2_frame_overview">ID3v2 Frame Overview</a>
+         */
+        @JvmStatic
+        fun languageFormat(language: String) = require(language matches Regex("[a-zA-Z]{3}")) {
+            "Language must follow ISO 639-2"
+        }
     }
 }
