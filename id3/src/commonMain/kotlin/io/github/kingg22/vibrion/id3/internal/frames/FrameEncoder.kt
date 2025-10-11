58c5bca309d7965004ad009726fb078b2cc1725c
Rey <89825425+Kingg22@users.noreply.github.com>
2025-10-11 16:46:19 -0500

    perf(vibrion-id3): [internal] make abstract and not sealed FrameEncoder, micro optimization of bytecode

--- id3/src/commonMain/kotlin/io/github/kingg22/vibrion/id3/internal/frames/FrameEncoder.kt
+++ id3/src/commonMain/kotlin/io/github/kingg22/vibrion/id3/internal/frames/FrameEncoder.kt
@@ -1,28 +1,22 @@
-@file:JvmMultifileClass
-@file:JvmSynthetic
-@file:JvmName("-FrameEncoder")
-
 package io.github.kingg22.vibrion.id3.internal.frames
 
 import io.github.kingg22.vibrion.id3.internal.encodeWindows1252
 import io.github.kingg22.vibrion.id3.internal.uint32ToUint8Array
-import kotlin.jvm.JvmMultifileClass
-import kotlin.jvm.JvmName
-import kotlin.jvm.JvmStatic
+import kotlin.jvm.JvmField
 import kotlin.jvm.JvmSynthetic
 
-internal sealed class FrameEncoder protected constructor(
-    @get:JvmSynthetic @field:JvmSynthetic protected open val name: String,
-    @get:JvmSynthetic @field:JvmSynthetic internal open val size: Int,
+internal abstract class FrameEncoder protected constructor(
+    name: String,
+    @get:JvmSynthetic @field:JvmSynthetic internal val size: Int,
 ) {
     private val encodedFrameHeader: ByteArray by lazy {
-        val buffer = ByteArray(HEADER)
+        val buffer = ByteArray(10)
         // FrameEncoder ID (4 bytes)
         val nameBytes = encodeWindows1252(name)
         nameBytes.copyInto(buffer, 0)
 
         // Content size (4 bytes, big-endian)
-        val frameSizeBytes = uint32ToUint8Array(size - HEADER)
+        val frameSizeBytes = uint32ToUint8Array(size - 10)
         frameSizeBytes.copyInto(buffer, 4)
 
         // Flags (2 bytes)
@@ -59,13 +53,11 @@ internal sealed class FrameEncoder protected constructor(
      */
     protected fun writeFrameHeader(buffer: ByteArray): Int {
         encodedFrameHeader.copyInto(buffer, 0)
-        return HEADER
+        return 10
     }
 
     protected companion object {
-        @JvmStatic
+        @JvmField
         protected val BOM = byteArrayOf(0xFF.toByte(), 0xFE.toByte())
-
-        protected const val HEADER = 10
     }
 }
